64a37b87a19db72def68bc0cc233b3a4b1334195
minhtu1999 <duyminh65@gmail.com>
2019-08-25 13:00:15 +0700

    them 25 thang 8

--- resources/views/nguoi-ban-hang/chung.blade.php
+++ resources/views/nguoi-ban-hang/chung.blade.php
@@ -10,61 +10,65 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('public/css/chung.css') }}">
     </head>
-    <nav class="navbar navbar-default" role="navigation">
-      <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">SIT.TV</a>
-        </div>
-    
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Deal Hot</a></li>
-            <li><a href="#">Qua Tang</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">San Pham Cua SIT.TV <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Gia Dung</a></li>
-                <li><a href="#">Dien Tu</a></li>
-                <li><a href="#">Quan Ao</a></li>
-                <li><a href="#">Linh Kien</a></li>
-                <li><a href="#">May Tinh - Dien Thoai</a></li>
-                <li><a href="#">Sach - Vo - But</a></li>
-                <li><a href="#">May Quay - May Anh</a></li>
-                <li><a href="#">Hang Quoc Te</a></li>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">BANHANG</a>
+    </div>
+    <ul class="nav navbar-nav">
 
-              </ul>
-            </li>
-          </ul>
-          <form class="navbar-form navbar-left" role="search">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Search">
-            </div>
-            <button type="submit" class="btn btn-default">TimKiem</button>
-          </form>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Don Hang</a></li>
-            <li><a href="#">Ca Nhan</a></li>
-            
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div>
-    </nav>
+      <li class="active"><a href="{{ asset('ban-hang/home') }}">Home</a></li>
+      <li><a href="{{ asset('ban-hang/cac-don-hang') }}">Các đơn hàng</a></li>
+      <li><a href="{{ asset('ban-hang/them-san-pham') }}">Thêm sản phẩm</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><p><a href="{{ asset('dang-xuat') }}">LogOut</a></p></span></li>
+    </ul>
+  </div>
+</nav>
   <body>
-    <h1 class="text-center">Hello World</h1>
-     <p>Trang Người Bán Hàng</p>
-  <p><a href="{{ asset('ban-hang/home') }}">Home</a></p>
-  <p><a href="{{ asset('ban-hang/them-san-pham') }}">Thêm sản phẩm</a></p>
-  <p><a href="{{ asset('ban-hang/cac-don-hang') }}">Các đơn hàng</a></p>
-  <p><a href="{{ asset('dang-xuat') }}">Đăng Xuất</a></p>
-  @yield('noi-dung')
+  <h1 class="text-center">Trang Người Bán Hàng</h1>
+  <div class="container">
+    <div class="danhmuc-right">
+      <div class="tieude">
+        <h1>Danh Mục Sản Phẩm</h1>
+        <table class="table">
+      <thead>
+        <tr>
+          <th>TênSảnPhẩm</th>
+          <th>TìnhTrạng</th>
+          <th>NgàyBán</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>DT</td>
+          <td>New99%</td>
+          <td>25/08/19</td>
+
+        </tr>
+        <tr>
+          <td>TuLạnh</td>
+          <td>Cũ</td>
+          <td>22/08/19</td>
+          
+        </tr>
+        <tr>
+          <td>Dieuhoa</td>
+          <td>New100%</td>
+          <td>25/08/19</td>
+        </tr>
+      </tbody>
+    </table>
+      </div>
+      
+    </div>
+    <ul class="pager">
+      <li><a href="#">Previous</a></li>
+      <li><a href="#">Next</a></li>
+    </ul>
+@yield('noi-dung')
+</div>  
     <!-- jQuery -->
     <script src="//code.jquery.com/jquery.js"></script>
     <!-- Bootstrap JavaScript -->
--- resources/views/nguoi-ban-hang/them-san-pham.blade.php
+++ resources/views/nguoi-ban-hang/them-san-pham.blade.php
@@ -8,11 +8,11 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-<<<<<<< HEAD
+
         <link rel="stylesheet" type="text/css" href="{{ asset('public/css/them-san-pham.css')}} ">
-=======
+
         {{--<link rel="stylesheet" type="text/css" href=" {{ asset('public/css/them-san-pham.css') }}">--}}
->>>>>>> 375c8dc5bc7d10d39595be9eb1c3d92cd827b338
+
        
     </head>
     <body>
